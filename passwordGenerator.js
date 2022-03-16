38bac687f17f4d73421961ed06efaea4761b94be
ChicoDeLosGatos <58256428+ChicoDeLosGatos@users.noreply.github.com>
2022-03-16 13:25:23 +0100

    Update passwordGenerator.js

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,84 +1,26 @@
 /**
 	v0.1
 */
-
-function generatePassword(length = 8, wds, it=false) {
-    const maxSyms = Math.floor(length / 2),
-        str = len => {
-            let res = '',
-                chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
-                charLen = chars.length;
-            for (let i = 0; i < len; i++)
-                res += chars.charAt(Math.floor(Math.random() * charLen));
-            return res;
-        },
-        ww = w => {
-            if (w) return w;
-            return 'lol,xdd,uwu,012,123,666,212,akira,akame,yago,wtf'.split(',')
-        },
-        maxLen = ww => {
-            let max = 0;
-            ww.forEach(w => max = (max > w.length) ? max : w.length)
-            return max;
-        },
-        randomSymbol = () => {
-            let s = "!\"$%&/()=?";
-            return s.substr(Math.floor(s.length * Math.random()), 1);
-        },
-        mix = (word, pass, sym) => {
-            let passwArr = pass.split(''),
-                passwLen = passwArr.length,
-                wordArr = word.split(''),
-                wordLen = wordArr.length,
-                passwIdx = Math.floor(Math.random() * passwLen),
-                passwCarry = passwIdx - wordLen,
-                wordIdx = Math.floor(Math.random() * wordLen),
-                firstChar = btoa(pass[0]).split('')[0];
-
-            passwCarry = (passwCarry <= 2) ? 2 : passwCarry;
-            passwArr.splice(passwCarry, wordLen);
-
-            let wordLett = '',
-                times = 0;
-
-            passwLen = passwArr.length,
-                passwIdx = Math.floor(Math.random() * passwLen);
-
-            for (let i = 0; i < wordLen; i++)
-                wordLett += (i == wordIdx) ? ((i == 0) ? wordArr[i] + firstChar : firstChar + wordArr[i]) : wordArr[i];
-
-            let result = [];
-
-            for (let i = 0; i < passwLen; i++)
-                if (i == passwIdx) {
-                    if (i == 0) {
-                        result.push(passwArr[i]);
-                        result.push(wordLett);
-                    } else {
-                        result.push(wordLett);
-                        result.push(passwArr[i]);
-                    }
-                } else if (i % (Math.floor(passwLen / 3)) == 0 && times < maxSyms && i > 0) {
-                result.push(sym);
-                times++;
-            } else result.push(passwArr[i]);
-
-            return result.join('');
-        }
-
-    if (length < maxLen(ww(wds))) return Error(`Minimum password length is ${maxLen(ww(wds))}`);
-
-    let nonCryptedStr = str(length),
-        cryptedStr = btoa(nonCryptedStr),
-        words = ww(wds),
-        wLen = words.length,
-        wIdx = Math.floor(Math.random() * wLen),
-        word = words[wIdx],
-	sym=randomSymbol();
-	
-	if(!it) return mix(word, cryptedStr, sym).substr(0, length).replace(' ','-').replace(' ','_');
-	return {password: mix(word, cryptedStr, sym).substr(0, length).replace(' ','-').replace(' ','_'), key: btoa(JSON.stringify({ncs:nonCryptedStr, w:word, s:sym}))}
+function Password (len=8, ws) {
+  this.length = len;
+  this.words = ws;
+  this.key = '';
 }
 
+Password.prototype.internal_generate = function () {
+   const t=this.length,r=this.words,e=true,a=Math.floor(t/2),l=t=>t||"lol,xdd,uwu,012,123,666,212,akira,akame,yago,wtf".split(","),o=t=>{let r=0;return t.forEach(t=>r=r>t.length?r:t.length),r},h=(t,r,e)=>{let l=r.split(""),o=l.length,h=t.split(""),n=h.length,s=Math.floor(Math.random()*o),u=s-n,p=Math.floor(Math.random()*n),f=btoa(r[0]).split("")[0];u=u<=2?2:u,l.splice(u,n);let M="",i=0;o=l.length,s=Math.floor(Math.random()*o);for(let t=0;t<n;t++)M+=t==p?0==t?h[t]+f:f+h[t]:h[t];let g=[];for(let t=0;t<o;t++)t==s?0==t?(g.push(l[t]),g.push(M)):(g.push(M),g.push(l[t])):t%Math.floor(o/3)==0&&i<a&&t>0?(g.push(e),i++):g.push(l[t]);return g.join("")};if(t<o(l(r)))return Error(`Minimum password length is ${o(l(r))}`);let n=(t=>{let r="",e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",a=e.length;for(let l=0;l<t;l++)r+=e.charAt(Math.floor(Math.random()*a));return r})(t),s=btoa(n),u=l(r),p=u.length,f=u[Math.floor(Math.random()*p)],M=(()=>{let t='!"$%&/()=?';return t.substr(Math.floor(t.length*Math.random()),1)})();return e?{password:h(f,s,M).substr(0,t).replace(" ","-").replace(" ","_"),key:btoa(JSON.stringify({ncs:n,w:f,s:M}))}:h(f,s,M).substr(0,t).replace(" ","-").replace(" ","_")
+}
+Password.prototype.generate = function () {
+  let password = this.internal_generate();
+  this.key = password.key;
+  return password.password;
+}
 
-generatePassword(64, ['I can easily', 'this phrases or', 'words', 'and the pass', 'will contain', 'one word of', 'this list']);
+Password.prototype.check = function (password, key) {
+    const params = JSON.parse(atob(key));
+    return params;    
+}
+
+p = new Password();
+p.generate();
+
